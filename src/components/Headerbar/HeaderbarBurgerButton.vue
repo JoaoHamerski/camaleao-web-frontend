f78f44c372ae600ac963df5392e36ee11575fda7
João Antônio <joaohamerski@hotmail.com>
2021-09-13 15:55:30 -0300

    ✨ Add new Hamburger Button to headerbar

---
+++ src/components/Headerbar/HeaderbarBurgerButton.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    isActive: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<template>
+  <button
+    class="hamburger hamburger-sm hamburger--squeeze"
+    :class="isActive && 'is-active'"
+    type="button"
+    v-on="$listeners"
+  >
+    <span class="hamburger-box">
+      <span class="hamburger-inner" />
+    </span>
+  </button>
+</template>
+
+<style scoped>
+.hamburger-sm {
+  transform: scale(0.65)
+}
+</style>
