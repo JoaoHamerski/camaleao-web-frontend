f59a3a8c0e4524da186581b72ba3fc928ea73027
João Antônio <joaohamerski@hotmail.com>
2021-09-12 21:58:07 -0300

    🚚 Rename auth routes

--- src/views/auth/routes.js
+++ src/views/auth/routes.js
@@ -1,17 +1,8 @@
-import Layout from './Layout'
 import TheLogin from './TheLogin'
-
-const children = [
-  {
-    path: '/entrar',
-    component: TheLogin
-  }
-]
 
 export default [
   {
-    path: '',
-    component: Layout,
-    children
+    path: '/entrar',
+    component: TheLogin
   }
 ]
