5a2a6a947d2c1447b674824aba3107516609efa6
João Antônio <joaohamerski@hotmail.com>
2021-09-13 15:58:23 -0300

    ✨ Integrate Hamburger Button to Headerbar

--- src/components/Headerbar/TheHeaderbar.vue
+++ src/components/Headerbar/TheHeaderbar.vue
@@ -1,9 +1,35 @@
+<script>
+import HeaderbarBurgerButton from './HeaderbarBurgerButton.vue'
+
+export default {
+  components: {
+    HeaderbarBurgerButton
+  },
+  computed: {
+    isSidebarActive () {
+      return this.$store.getters['sidebar/isSidebarActive']
+    }
+  },
+  methods: {
+    toggleSidebarState () {
+      this.$store.commit('sidebar/SET_SIDEBAR_STATE', !this.isSidebarActive, { root: true })
+    }
+  }
+}
+</script>
+
 <template>
   <nav
     id="headerbar"
-    class="bg-white py-2"
+    class="d-flex bg-white align-items-center py-2"
   >
-    <div class="col-1 ms-5">
+    <HeaderbarBurgerButton
+      class="mx-2"
+      :is-active="isSidebarActive"
+      @click="toggleSidebarState"
+    />
+
+    <div class="col-1 ms-3">
       <img
         class="img-fluid"
         src="@/assets/images/logo.png"
