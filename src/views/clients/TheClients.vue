18961dcf323ba41620ab6be5b200ac65294f8f6c
João Antônio <joaohamerski@hotmail.com>
2021-09-12 21:54:49 -0300

    💄 Add clients view

---
+++ src/views/clients/TheClients.vue
@@ -0,0 +1,36 @@
+<script>
+import { faList } from '@fortawesome/free-solid-svg-icons'
+
+export default {
+  metaInfo: {
+    title: 'Clientes'
+  },
+  data () {
+    return {
+      faList
+    }
+  }
+}
+</script>
+
+<template>
+  <AppCard
+    color="primary"
+    class="mt-5"
+  >
+    <template #header>
+      <h6 class="fw-bold mb-0">
+        <FontAwesomeIcon
+          :icon="faList"
+          fixed-width
+          class="me-1"
+        />
+        Lista de clientes
+      </h6>
+    </template>
+
+    <template #body>
+      <!--  -->
+    </template>
+  </AppCard>
+</template>
