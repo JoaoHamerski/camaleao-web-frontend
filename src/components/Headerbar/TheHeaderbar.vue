c34605e8592c54ee71a3e907eff7c5ad6d8758fb
João Antônio <joaohamerski@hotmail.com>
2021-09-12 21:53:21 -0300

    💄 Add headerbar

---
+++ src/components/Headerbar/TheHeaderbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav
+    id="headerbar"
+    class="bg-white py-2"
+  >
+    <div class="col-1 ms-5">
+      <img
+        class="img-fluid"
+        src="@/assets/images/logo.png"
+        alt=""
+      >
+    </div>
+
+    <!--  -->
+  </nav>
+</template>
