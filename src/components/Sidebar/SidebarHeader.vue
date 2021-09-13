da0e75683b93a422d7c5012c4493d79929f51b6d
João Antônio <joaohamerski@hotmail.com>
2021-09-13 15:59:31 -0300

    💄 Add SidebarHeader on individual file

---
+++ src/components/Sidebar/SidebarHeader.vue
@@ -0,0 +1,31 @@
+<script>
+import { mapGetters } from 'vuex'
+import {
+  faUserCircle
+} from '@fortawesome/free-solid-svg-icons'
+
+export default {
+  data () {
+    return {
+      icons: {
+        faUserCircle
+      }
+    }
+  },
+  computed: {
+    ...mapGetters('auth', { user: 'authUser' })
+  }
+}
+</script>
+<template>
+  <div class="text-center my-4">
+    <FontAwesomeIcon
+      :icon="icons.faUserCircle"
+      class="text-white"
+      size="3x"
+    />
+    <h5 class="text-white mt-3">
+      {{ user.name }}
+    </h5>
+  </div>
+</template>
