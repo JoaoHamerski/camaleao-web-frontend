523cd8daea42695fb5da65e6d97f839001622e51
João Antônio <joaohamerski@hotmail.com>
2021-09-12 22:00:33 -0300

    🎨 Adjustments on router index

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import store from '@/store'
+
 import authRoutes from '@/views/auth/routes'
+import clientsRoutes from '@/views/clients/routes'
 
 Vue.use(VueRouter)
 
 const routes = [
-  ...authRoutes
+  {
+    path: '/',
+    beforeEnter: (to, from, next) => next('/clientes')
+  },
+  ...authRoutes,
+  ...clientsRoutes
 ]
 
 const router = new VueRouter({
@@ -17,11 +24,11 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   const authUser = store.getters['auth/authUser']
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-  const loginQuery = { path: '/login', query: { redirect: to.fullPath } }
+  const loginQuery = { path: '/entrar' }
 
   if (requiresAuth && !authUser) {
     store.dispatch('auth/getAuthUser').then(() => {
-      if (!store.getters(['auth/authUser'])) {
+      if (!store.getters['auth/authUser']) {
         next(loginQuery)
       } else {
         next()
