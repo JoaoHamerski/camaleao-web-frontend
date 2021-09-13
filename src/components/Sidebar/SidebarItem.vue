59de44ef35ea9142bb8c8523149d527e4f52136e
João Antônio <joaohamerski@hotmail.com>
2021-09-12 21:52:52 -0300

    ✨ Add sidebar

---
+++ src/components/Sidebar/SidebarItem.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  props: {
+    icon: {
+      type: Object,
+      default: null
+    },
+    to: {
+      type: [String, Object],
+      default: ''
+    }
+  }
+}
+</script>
+
+<template>
+  <router-link
+    v-slot="{ navigate, isExactActive }"
+    custom
+    :to="to"
+  >
+    <li
+      class="list-group-item list-sidebar-item px-4 clickable"
+      :class="{'active': isExactActive}"
+      @click="navigate"
+    >
+      <FontAwesomeIcon
+        v-if="icon"
+        :icon="icon"
+        fixed-width
+        class="me-1 text-primary"
+      />
+
+      <slot />
+    </li>
+  </router-link>
+</template>
---
+++ src/components/Sidebar/TheSidebar.vue
@@ -0,0 +1,53 @@
+<script>
+import { mapGetters } from 'vuex'
+
+import SidebarItem from './SidebarItem.vue'
+import {
+  faList,
+  faUserCircle
+} from '@fortawesome/free-solid-svg-icons'
+
+export default {
+  components: {
+    SidebarItem
+  },
+  data () {
+    return {
+      icons: {
+        faList,
+        faUserCircle
+      }
+    }
+  },
+  computed: {
+    ...mapGetters('auth', { user: 'authUser' })
+  }
+}
+</script>
+
+<template>
+  <nav
+    id="sidebar"
+    class="active"
+  >
+    <div class="text-center my-4">
+      <FontAwesomeIcon
+        :icon="icons.faUserCircle"
+        class="text-white"
+        size="3x"
+      />
+      <h5 class="text-white mt-3">
+        {{ user.name }}
+      </h5>
+    </div>
+    <hr class="bg-light">
+    <ul class="list-group list-group-flush">
+      <SidebarItem
+        :icon="icons.faList"
+        :to="{name: 'clients.index'}"
+      >
+        Clientes
+      </SidebarItem>
+    </ul>
+  </nav>
+</template>
