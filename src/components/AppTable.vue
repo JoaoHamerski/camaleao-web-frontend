801a789b6b6ce1225e8cd7477b8d7bca58df291b
João Antônio <joaohamerski@hotmail.com>
2021-09-18 18:56:11 -0300

    ✨ Add AppTable component

---
+++ src/components/AppTable.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  props: {
+    headers: {
+      type: Array,
+      default: () => []
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    hasSlot (name) {
+      return this.$slots[name] || this.$scopedSlots[name]
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+          <th
+            v-for="header in headers"
+            :key="header.value"
+          >
+            {{ header.text }}
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr
+          v-for="item in items"
+          :key="item.id"
+        >
+          <td
+            v-for="header in headers"
+            :key="header.value"
+          >
+            <template v-if="!hasSlot(`items.${header.value}`)">
+              {{ item[header.value] }}
+            </template>
+            <slot
+              v-else
+              :name="`items.${header.value}`"
+              :item="item"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
