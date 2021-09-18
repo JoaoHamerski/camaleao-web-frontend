72e3eb05ed92b38bdee4e61a0199429862962fc4
João Antônio <joaohamerski@hotmail.com>
2021-09-18 18:53:01 -0300

    🎨 Split client request config from AuthService file

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,36 +1,15 @@
-import axios from 'axios'
-import store from '@/store'
-
-export const authClient = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-  withCredentials: true
-})
-
-authClient.interceptors.response.use(
-  response => response,
-  error => {
-    if (
-      error.response
-      && [401, 419].includes(error.response.status)
-      && store.getters['auth/authUser']
-      && !store.getters['auth/guest']
-    ) {
-      store.dispatch('auth/logout')
-    }
-
-    return Promise.reject(error)
-  })
+import client from '@/request'
 
 export default {
   async login (payload) {
-    await authClient.get('/api/csrf-cookie')
+    await client.get('/api/csrf-cookie')
 
-    return authClient.post('/login', payload)
+    return client.post('/login', payload)
   },
   logout () {
-    return authClient.post('/logout')
+    return client.post('/logout')
   },
   getAuthUser () {
-    return authClient.get('/api/users/auth')
+    return client.get('/api/users/auth')
   }
 }
