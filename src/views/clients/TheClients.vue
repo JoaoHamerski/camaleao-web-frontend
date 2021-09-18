bc51bdb1888e954692b2721694a3f1d3bf0850eb
João Antônio <joaohamerski@hotmail.com>
2021-09-18 18:56:39 -0300

    🎨 Integrate backend with clients index view

--- src/views/clients/TheClients.vue
+++ src/views/clients/TheClients.vue
@@ -1,36 +1,94 @@
 <script>
 import { faList } from '@fortawesome/free-solid-svg-icons'
+import { maskPhone } from '@/utils/masks'
 
 export default {
   metaInfo: {
     title: 'Clientes'
   },
+  chimera: {
+    clients: '/api/clients',
+    params () { return { page: this.page } }
+  },
   data () {
     return {
-      faList
+      maskPhone,
+      faList,
+      isLoading: false,
+      clients: [],
+      pagination: {},
+      page: 2
+    }
+  },
+  computed: {
+    headers () {
+      return [
+        { value: 'name', text: 'Nome' },
+        { value: 'phone', text: 'Telefone' },
+        { value: 'city', text: 'Cidade' }
+      ]
+    }
+  },
+  methods: {
+    paginate (page) {
+      this.page = page
     }
   }
 }
 </script>
 
 <template>
-  <AppCard
-    color="primary"
-    class="mt-5"
-  >
-    <template #header>
-      <h6 class="fw-bold mb-0">
-        <FontAwesomeIcon
-          :icon="faList"
-          fixed-width
-          class="me-1"
-        />
-        Lista de clientes
-      </h6>
-    </template>
+  <div class="col-10 mx-auto">
+    <AppCard
+      :has-body-padding="false"
+      color="camaleao"
+      class="mt-5"
+    >
+      <template #header>
+        <h6 class="fw-bold mb-0">
+          <FontAwesomeIcon
+            :icon="faList"
+            fixed-width
+            class="me-1"
+          />
+          Lista de clientes
+        </h6>
+      </template>
 
-    <template #body>
-      <!--  -->
-    </template>
-  </AppCard>
+      <template #body>
+        <AppTable
+          v-if="clients.length"
+          :headers="headers"
+          :items="clients"
+        >
+          <template #[`items.city`]="{ item }">
+            {{ $helpers.fallback(item.city, 'name') }}
+          </template>
+
+          <template #[`items.phone`]="{item}">
+            {{ maskPhone(item.phone) }}
+          </template>
+        </AppTable>
+
+        <nav aria-label="...">
+          <ul class="pagination">
+            <li
+              v-for="(link, index) in pagination.links"
+              :key="index"
+              class="page-item"
+              :class="{
+                'active': link.active,
+                'disabled': link.url === null
+              }"
+            >
+              <a
+                class="page-link clickable"
+                @click="paginate(link.label)"
+              >{{ link.label }}</a>
+            </li>
+          </ul>
+        </nav>
+      </template>
+    </AppCard>
+  </div>
 </template>
