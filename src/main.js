d38f9149d32daa14e901781dc173d41a7877e5b9
João Antônio <joaohamerski@hotmail.com>
2021-09-18 18:53:20 -0300

    🎨 Split vue plugins from main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import './sass/app.scss'
 import '@/components'
+import './plugins'
 import Vue from 'vue'
-import VueMeta from 'vue-meta'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.use(VueMeta)
 Vue.config.productionTip = false
 
 store.dispatch('sidebar/bootstrap')
---
+++ src/plugins.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueMeta from 'vue-meta'
+import VueChimera from 'vue-chimera'
+import client from '@/request'
+import helpers from '@/utils/helpers'
+
+const custom = {
+  install (Vue) {
+    Vue.prototype.$helpers = helpers
+  }
+}
+
+Vue.use(VueMeta)
+Vue.use(custom)
+
+Vue.use(VueChimera, {
+  axios: client
+})
