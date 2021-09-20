e8761ac523c7b22c6364349f7146eabe37fe209a
João Antônio <joaohamerski@hotmail.com>
2021-09-20 16:57:50 -0300

    ✨ Add new AppSelect component

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<template>
+  <select
+    class="form-select"
+    v-bind="$attrs"
+    @change="$emit('input', $event.target.value)"
+    v-on="$listeners"
+  >
+    <option
+      v-for="(option, index) in options"
+      :key="index"
+      :value="option.value ? option.value : option.text.toLowerCase()"
+    >
+      {{ option.text }}
+    </option>
+  </select>
+</template>
