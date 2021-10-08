9260b6bdf4cd04e4831e805ef65529afde90491e
João Antônio <joaohamerski@hotmail.com>
2021-10-08 19:18:40 -0300

    ✨ Add goBack button to headerbar

--- src/components/Headerbar/TheHeaderbar.vue
+++ src/components/Headerbar/TheHeaderbar.vue
@@ -1,15 +1,16 @@
 <script>
 import SVGInject from '@iconfu/svg-inject'
 import HeaderbarBurgerButton from './HeaderbarBurgerButton.vue'
+import HeaderbarBackButton from './HeaderbarBackButton.vue'
 
 export default {
   components: {
-    HeaderbarBurgerButton
+    HeaderbarBurgerButton,
+    HeaderbarBackButton
   },
   data () {
     return {
-      SVGInject,
-      console
+      SVGInject
     }
   },
   computed: {
@@ -28,22 +29,25 @@ export default {
 <template>
   <nav
     id="headerbar"
-    class="d-flex align-items-center py-2"
+    class="d-flex justify-content-between align-items-center py-2"
   >
-    <HeaderbarBurgerButton
-      class="mx-2"
-      :is-active="isSidebarActive"
-      @click="toggleSidebarState"
-    />
-
-    <div class="col-1 ms-3">
-      <img
-        class="img-fluid logo"
-        src="@/assets/images/logo.svg"
-        @load="SVGInject($event.target)"
-      >
+    <div class="d-flex">
+      <HeaderbarBurgerButton
+        class="mx-2"
+        :is-active="isSidebarActive"
+        @click="toggleSidebarState"
+      />
+      <div class="col-4 ms-3">
+        <img
+          class="img-fluid logo"
+          src="@/assets/images/logo.svg"
+          @load="SVGInject($event.target)"
+        >
+      </div>
     </div>
 
-    <!--  -->
+    <div class="text-right">
+      <HeaderbarBackButton />
+    </div>
   </nav>
 </template>
