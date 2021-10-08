711f0b53391f897ea2d4aa3fc9e1d02cb725ca57
João Antônio <joaohamerski@hotmail.com>
2021-10-08 19:16:35 -0300

    🎨 Improve helpers & add toBRL helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,23 @@
+/**
+ * @file Arquivo para o plugin de helpers do vue disponível
+ * através de $helpers variavels
+ */
 import { isNil } from 'lodash-es'
+import { formatMoney } from '@/utils/formatters'
 
 const fallback = (object, prop, fallbackString = 'N/A') => {
+  if (isNil(prop)) {
+    return object || fallbackString
+  }
+
   return isNil(object) ? fallbackString : object[prop]
 }
 
+const toBRL = (str) => {
+  return formatMoney(str)
+}
+
 export default {
-  fallback
+  fallback,
+  toBRL
 }
