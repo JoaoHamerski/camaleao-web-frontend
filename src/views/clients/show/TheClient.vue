488514f884f696e7fc79badf17cc04cca4b716d3
João Antônio <joaohamerski@hotmail.com>
2021-10-14 18:50:54 -0300

    🎨 Improvements on client.show

--- src/views/clients/show/TheClient.vue
+++ src/views/clients/show/TheClient.vue
@@ -1,6 +1,6 @@
 <script>
 import ClientCard from '../partials/ClientCard'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons'
 import 'tippy.js/themes/light-border.css'
 
 import ClientOrdersCard from './ClientOrdersCard'
@@ -16,10 +16,7 @@ export default {
     _orders () {
       return {
         url: `/api/clients/${this.$route.params.client}/orders/`,
-        params: {
-          page: this.page,
-          code: this.code
-        }
+        params: { page: this.page }
       }
     }
   },
@@ -28,11 +25,15 @@ export default {
       page: 1,
       code: '',
       icons: {
-        faPlus
+        faPlus,
+        faArrowAltCircleLeft
       }
     }
   },
   computed: {
+    isLoading () {
+      return this.$chimera._orders.loading
+    },
     clientId () {
       return this.$route.params.client
     },
@@ -42,14 +43,24 @@ export default {
     pagination () {
       return this.$chimera._orders?.data?.meta || ({})
     }
-
   },
   methods: {
+    redirectToOrder () {
+
+    },
     handleSearch (code) {
       this.code = code
+
+      this.$chimera._orders.fetch(true, {
+        params: { code, page: 1 }
+      })
     },
     onSearchClear () {
       this.code = ''
+
+      this.$chimera._orders.fetch(true, {
+        params: { code: '', page: 1 }
+      })
     }
   }
 }
@@ -58,6 +69,15 @@ export default {
 <template>
   <div class="mt-5 row mx-auto">
     <div class="col-3">
+      <div class="mb-1">
+        <AppButton
+          outlined
+          @click="$router.push({name: 'clients.index'})"
+        >
+          <FontAwesomeIcon :icon="icons.faArrowAltCircleLeft" />
+          Clientes
+        </AppButton>
+      </div>
       <ClientCard
         :client-id="clientId"
       />
@@ -70,7 +90,10 @@ export default {
         @clear-search="onSearchClear"
       />
 
-      <ClientOrdersCard :orders="orders" />
+      <ClientOrdersCard
+        :is-loading="isLoading"
+        :orders="orders"
+      />
 
       <AppPaginator
         class="mt-2"
