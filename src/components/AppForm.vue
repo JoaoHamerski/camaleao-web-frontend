786f34f6dd6e34de1c135c04b3f382c595372bab
João Antônio <joaohamerski@hotmail.com>
2021-10-14 18:40:19 -0300

    ✨ Add new AppForm component

---
+++ src/components/AppForm.vue
@@ -0,0 +1,26 @@
+<script>
+import Form from '@/utils/Form'
+
+export default {
+  props: {
+    form: {
+      type: Form,
+      required: true
+    },
+    onSubmit: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <form
+    @submit.prevent="onSubmit"
+    @keypress.enter.prevent="onSubmit"
+    @focus.capture="form.errors.clear($event.target.name)"
+  >
+    <slot />
+  </form>
+</template>
