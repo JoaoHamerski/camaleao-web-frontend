451413e68b0d428c09d334fe205f182791dcce46
João Antônio <joaohamerski@hotmail.com>
2021-10-28 14:02:26 -0300

    🎨 Add order body components content

---
+++ src/views/orders/show/OrderCardBody.vue
@@ -0,0 +1,115 @@
+<script>
+import OrderClothingTypesTable from './OrderClothingTypesTable'
+import OrderPayments from './OrderPayments'
+
+export default {
+  components: {
+    OrderClothingTypesTable,
+    OrderPayments
+  },
+  props: {
+    order: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- eslint-disable vue/no-v-html -->
+
+  <div>
+    <div>
+      <h5 class="fw-bold text-secondary">
+        Detalhes do Pedido
+      </h5>
+      <div class="d-flex justify-content-between">
+        <div>
+          <div class="fw-bold small text-secondary">
+            Nome
+          </div>
+          <b class="text-subtitle">
+            {{ $helpers.fallback(order, 'name') }}
+          </b>
+        </div>
+        <div>
+          <div class="fw-bold small text-secondary">
+            Código
+          </div>
+          <b class="text-subtitle">
+            {{ order.code }}
+          </b>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex justify-content-between mt-3">
+      <div>
+        <b class="small text-secondary">Status</b>
+        <h5
+          class="fw-bold"
+          :class="order.status.avaliable ? 'text-success' : 'text-warning'"
+        >
+          {{ order.status.text }}
+        </h5>
+      </div>
+      <div>
+        <b class="small text-secondary">Quantidade</b>
+        <h5 class="fw-bold text-center">
+          {{ order.quantity }}
+        </h5>
+      </div>
+    </div>
+    <div class="mt-3 d-flex justify-content-between">
+      <div>
+        <b class="small text-secondary">Data de produção</b>
+        <div>
+          {{ $helpers.fallback(order.production_date) }}
+        </div>
+      </div>
+      <div>
+        <b class="small text-secondary">Data de entrega</b>
+        <div>
+          {{ $helpers.fallback(order.delivery_date) }}
+        </div>
+      </div>
+    </div>
+    <div class="mt-3">
+      <div>
+        <b class="small text-secondary">Tipos de roupa</b>
+      </div>
+      <OrderClothingTypesTable :order="order" />
+    </div>
+    <div class="mt-3 d-flex justify-content-around">
+      <div>
+        <div>
+          <b class="small text-secondary">Total pago</b>
+        </div>
+        <h5
+          :class="{
+            'text-success': order.total_paid >= 0,
+          }"
+          v-html="$helpers.toBRL(order.total_paid, true)"
+        />
+      </div>
+      <div>
+        <div>
+          <b class="small text-secondary">Faltar pagar</b>
+        </div>
+        <h5
+          :class="{
+            'text-success': order.total_owing <= 0,
+            'text-danger': order.total_owing > 0
+          }"
+          v-html="$helpers.toBRL(order.total_owing, true)"
+        />
+      </div>
+    </div>
+    <div class="mt-5">
+      <h5 class="fw-bold text-secondary">
+        Pagamentos
+      </h5>
+      <OrderPayments :payments="order.payments" />
+    </div>
+  </div>
+</template>
---
+++ src/views/orders/show/OrderCardBodyItem.vue
@@ -0,0 +1,46 @@
+<script>
+import { isEmpty } from 'lodash-es'
+
+export default {
+  props: {
+    label: {
+      type: String,
+      default: null
+    },
+    text: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: String,
+      default: null
+    }
+  },
+  methods: {
+    isEmpty
+  }
+}
+</script>
+
+<template>
+  <div>
+    <b class="small text-secondary">
+      <template v-if="$slots.label">
+        <slot name="label" />
+      </template>
+      <template v-else> {{ label }}</template>
+    </b>
+
+    <template>
+      <template v-if="$slots.text">
+        <slot name="text" />
+      </template>
+      <div
+        v-else-if="!isEmpty(text)"
+        class=""
+      >
+        {{ text }}
+      </div>
+    </template>
+  </div>
+</template>
