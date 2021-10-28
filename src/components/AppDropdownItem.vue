eeb8ab136ebc3f54c9517eedd912c22808ab722e
João Antônio <joaohamerski@hotmail.com>
2021-10-28 13:45:13 -0300

    ✨ Add AppDrodownItem component

---
+++ src/components/AppDropdownItem.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: Object,
+      default: null
+    },
+    itemClass: {
+      type: String,
+      default: ''
+    },
+    iconColor: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      active: false
+    }
+  }
+}
+</script>
+<template>
+  <li v-on="$listeners">
+    <a
+      class="dropdown-item"
+      :class="itemClass"
+      href="#"
+      @mousedown="active = true"
+      @mouseup="active = false"
+    >
+      <FontAwesomeIcon
+        v-if="icon"
+        :class="{
+          [`text-${iconColor}`]: !active,
+          'text-white': active
+        }"
+        fixed-width
+        :icon="icon"
+      />
+
+      <template v-if="$slots.text">
+        <slot name="text" />
+      </template>
+      <template v-else>
+        {{ text }}
+      </template>
+    </a>
+  </li>
+</template>
