32f5de6e1b1bb2699b0f34baca74c54e9edf8215
João Antônio <joaohamerski@hotmail.com>
2021-10-28 13:59:02 -0300

    ✨ Add create order route

--- src/views/orders/routes.js
+++ src/views/orders/routes.js
@@ -4,8 +4,18 @@ import role from '@/middleware/role'
 import roles from '@/constants/roles'
 
 import TheOrder from './show/TheOrder'
+import TheOrderNew from './create/TheOrderNew'
 
 const children = [
+  {
+    name: 'orders.create',
+    path: '/clientes/:client/novo-pedido',
+    component: TheOrderNew,
+    meta: {
+      middleware: [role],
+      roles: [roles.ATENDIMENTO, roles.GERENCIA]
+    }
+  },
   {
     name: 'orders.show',
     path: '/clientes/:client/pedidos/:order',
