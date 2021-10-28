2ab9fe09fc483059269d97710a6f0f859b42084b
João Antônio <joaohamerski@hotmail.com>
2021-10-28 13:42:39 -0300

    ✨ Add VueTippy as plugin

--- src/plugins.js
+++ src/plugins.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueMeta from 'vue-meta'
 import VueChimera from 'vue-chimera'
 import VueToast from 'vue-toast-notification'
+import VueTippy from 'vue-tippy'
 import client from '@/request'
 import helpers from '@/utils/helpers'
 import 'cleave.js/dist/addons/cleave-phone.br'
@@ -12,11 +13,17 @@ const custom = {
   }
 }
 
+Vue.use(VueTippy, {
+  duration: 150,
+  arrow: true
+})
+
 Vue.use(VueToast, {
   position: 'top-right'
 })
 
 Vue.use(VueMeta)
+
 Vue.use(custom)
 Vue.use(VueChimera, {
   axios: client
