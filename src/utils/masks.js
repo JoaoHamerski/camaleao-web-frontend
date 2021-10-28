c513118a1984974a9d507c8a57fcdf67e8680dc3
João Antônio <joaohamerski@hotmail.com>
2021-10-28 13:51:35 -0300

    🎨 Add mask to BRL monetary value & date mask adjustments

--- src/utils/masks.js
+++ src/utils/masks.js
@@ -1,10 +1,26 @@
 export const maskDate = {
   date: true,
-  delimiter: '-',
-  datePattern: ['Y', 'm', 'd']
+  delimiter: '/',
+  datePattern: ['d', 'm', 'Y']
 }
 
 export const maskPhone = {
   phone: true,
   phoneRegionCode: 'br'
 }
+
+export const maskValueBRL = (options = {}) => ({
+  prefix: 'R$ ',
+  numeral: true,
+  numeralDecimalMark: ',',
+  delimiter: '.',
+  numeralDecimalScale: 2,
+  ...options
+})
+
+export const maskInteger = (options = {}) => ({
+  numeral: true,
+  delimiter: '.',
+  numeralIntegerScale: 5,
+  ...options
+})
