15f16c1cde5e4198dd05b1fc712092aa220149f9
João Antônio <joaohamerski@hotmail.com>
2021-11-10 18:47:25 -0300

    ✨ Add AppInputFile component

---
+++ src/components/AppInputFile/AppInputFile.vue
@@ -0,0 +1,130 @@
+<script>
+import InputFileButton from './InputFileButton'
+import InputFileDragLabel from './InputFileDragLabel'
+
+const DRAG_STATES = {
+  DRAG_LEAVE: 0,
+  DRAG_ENTER: 1
+}
+
+const preventDefault = e => { e.preventDefault() }
+
+export default {
+  components: {
+    InputFileButton,
+    InputFileDragLabel
+  },
+  props: {
+    optional: {
+      type: Boolean,
+      default: false
+    },
+    id: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: Array,
+      default: () => ([])
+    },
+    centered: {
+      type: Boolean,
+      default: false
+    },
+    multiple: {
+      type: Boolean,
+      default: false
+    },
+    accept: {
+      type: String,
+      default: null
+    },
+    error: {
+      type: [String, Boolean],
+      default: false
+    }
+  },
+  data () {
+    return {
+      DRAG_STATES,
+      dragState: DRAG_STATES.DRAG_LEAVE
+    }
+  },
+  mounted () {
+    const preventEvents = ['dragover', 'drop']
+
+    for (const event of preventEvents) {
+      window.addEventListener(event, preventDefault, false)
+    }
+  },
+  methods: {
+    onFileDrop (event) {
+      this.$emit('input', event.dataTransfer.files)
+      this.dragState = DRAG_STATES.DRAG_LEAVE
+    },
+    onInputChange (event) {
+      this.$emit('input', event.target.files)
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="mb-3">
+    <label
+      :for="id"
+      class="form-label"
+    >
+      <span class="fw-bold">
+        <slot v-if="$slots.default" />
+        <template v-else>
+          {{ label }}
+        </template>
+      </span>
+      <span
+        v-if="optional"
+        class="small text-secondary"
+      >(opcional)</span>
+    </label>
+
+    <div
+      class="file-input-area"
+      @drop.stop.prevent="onFileDrop"
+    >
+      <InputFileButton
+        v-if="dragState === DRAG_STATES.DRAG_LEAVE"
+        key="inputFileButton"
+        v-bind="{id, centered, error}"
+        @dragenter.prevent.stop="dragState = DRAG_STATES.DRAG_ENTER"
+      />
+      <InputFileDragLabel
+        v-else-if="dragState === DRAG_STATES.DRAG_ENTER"
+        key="inputFileDragLabel"
+        @dragleave.prevent.stop="dragState = DRAG_STATES.DRAG_LEAVE"
+      />
+    </div>
+
+    <div
+      v-if="error"
+      class="small text-danger"
+    >
+      {{ error }}
+    </div>
+
+    <input
+      :id="id"
+      ref="input"
+      class="d-none"
+      type="file"
+      :multiple="multiple"
+      :accept="accept"
+      @change="onInputChange"
+    >
+  </div>
+</template>
+
+<style scoped>
+.file-input-area {
+  min-height: 100px;
+}
+</style>
---
+++ src/components/AppInputFile/InputFileButton.vue
@@ -0,0 +1,60 @@
+<script>
+import { faUpload } from '@fortawesome/free-solid-svg-icons'
+
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    centered: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: [String, Boolean],
+      default: false
+    }
+  },
+  data () {
+    return {
+      icons: {
+        faUpload
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div
+    :class="{
+      'text-center': centered
+    }"
+    v-on="$listeners"
+  >
+    <label
+      :for="id"
+      class="btn"
+      :class="{
+        'btn-outline-primary': !error,
+        'btn-outline-danger': error,
+        'px-5': centered
+      }"
+    >
+      <FontAwesomeIcon
+        :icon="icons.faUpload"
+        :class="!centered && 'me-2'"
+      />
+      <br v-show="centered">
+      Enviar arquivo
+    </label>
+
+    <small
+      v-if="centered"
+      class="text-secondary d-block"
+    >
+      OU ARRASTE E SOLTE
+    </small>
+  </div>
+</template>
