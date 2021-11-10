3c257e4a9b202b7888ea66a18e596895d68f971b
João Antônio <joaohamerski@hotmail.com>
2021-11-10 18:50:53 -0300

    ✨ Add helper to convert input files to base64

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,6 @@
 /**
  * @file Arquivo para o plugin de helpers do vue disponível
- * através de $helpers variavels
+ * através de $helpers variavel
  */
 import { isNil } from 'lodash-es'
 import { formatCurrencyBRL } from '@/utils/formatters'
@@ -17,7 +17,33 @@ const toBRL = (str, highlightNumerator = false) => {
   return formatCurrencyBRL(str, highlightNumerator)
 }
 
+const inputFileToBase64 = file => new Promise((resolve, reject) => {
+  const reader = new FileReader()
+  reader.readAsDataURL(file)
+
+  reader.onload = () => resolve(reader.result)
+  reader.onerror = error => reject(error)
+})
+
+const strContainsAny = (str, search) => {
+  if (typeof search === 'object') {
+    for (const s of search) {
+      if (str.indexOf(s) > -1) {
+        return true
+      }
+    }
+  }
+
+  if (str.indexOf(search) > -1) {
+    return true
+  }
+
+  return false
+}
+
 export default {
   fallback,
-  toBRL
+  toBRL,
+  inputFileToBase64,
+  strContainsAny
 }
