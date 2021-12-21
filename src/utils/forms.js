4e2ef6de7a9bc3eb1b7ac512a564f1f2f02ae749
João Antônio <joaohamerski@hotmail.com>
2021-12-21 18:58:53 -0300

    🎨 Improvements on form handles file

--- src/utils/forms.js
+++ src/utils/forms.js
@@ -1,23 +1,23 @@
+import { has } from 'lodash-es'
+
 export const handleError = (context, error) => {
   context.$toast.error('Algo está incorreto, verifique os dados, por favor.', {
     duration: 4000
   })
 
-  if (error.errors) {
+  if (has(error, 'errors')) {
     context.form.onFail(error.errors)
   }
 }
 
-export const handleSuccess = (context, { message, closeModal }) => {
-  context.form.reset()
-
-  if (message) {
-    context.$toast.success(message)
+export const handleSuccess = (context, options) => {
+  if (has(options, 'resetForm')) {
+    context.form.reset()
   }
 
-  context.$emit('submitted')
-
-  if (closeModal) {
-    context.modal = false
+  if (has(options, 'message')) {
+    context.$toast.success(options.message)
   }
+
+  context.$emit('success')
 }
