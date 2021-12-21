bff448eca8c2519afd7de791a59628ab1e569549
João Antônio <joaohamerski@hotmail.com>
2021-12-21 19:10:35 -0300

    ✨ Add OrderAttachments component

---
+++ src/views/orders/show/partials/OrderAttachments.vue
@@ -0,0 +1,97 @@
+<script>
+import { keys, isEmpty } from 'lodash-es'
+
+const ATTACHMENTS_MAP = {
+  art_paths: 'Imagens da arte',
+  size_paths: 'Imagens do tamanho',
+  payment_voucher_paths: 'Comprovantes de pagamento'
+}
+
+export default {
+  props: {
+    order: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      ATTACHMENTS_MAP
+    }
+  },
+  computed: {
+    attachmentsCount () {
+      const fields = keys(ATTACHMENTS_MAP)
+
+      return fields.reduce((total, field) => {
+        const length = !isEmpty(this.order[field])
+          ? this.order[field].length
+          : 0
+
+        return length + total
+      }, 0)
+    }
+  }
+}
+</script>
+
+<template>
+  <AppCollapsible
+    v-if="attachmentsCount"
+    id="attachments"
+    header-class="text-secondary"
+  >
+    <template #header>
+      <h5 class="fw-bold">
+        Anexos <span v-if="attachmentsCount">({{ attachmentsCount }})</span>
+      </h5>
+    </template>
+
+    <template #body>
+      <div
+        v-for="(label, key) in ATTACHMENTS_MAP"
+        :key="key"
+      >
+        <div
+          v-if="order[key]"
+          class="mb-3"
+        >
+          <div class="text-secondary mb-1">
+            &bull; {{ label }}
+          </div>
+          <div v-if="order[key].length">
+            <AppViewerItems
+              :alt="label"
+              :attachments="order[key]"
+            />
+          </div>
+          <div
+            v-else
+            class="text-center text-secondary py-4"
+          >
+            Nenhum anexo registrado
+          </div>
+        </div>
+
+        <div v-else>
+          <div class="text-secondary mb-1">
+            &bull; {{ label }}
+          </div>
+          <div class="small text-center text-secondary py-5">
+            Nenhum anexo registrado
+          </div>
+        </div>
+      </div>
+    </template>
+  </AppCollapsible>
+  <div
+    v-else
+  >
+    <h5 class="fw-bold text-secondary">
+      Anexos
+    </h5>
+    <div class="text-secondary text-center py-3">
+      Nenhum anexo registrado
+    </div>
+  </div>
+</template>
