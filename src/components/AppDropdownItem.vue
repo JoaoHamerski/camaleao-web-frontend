bf1142030794566f285c3072791714e3b30b10fa
João Antônio <joaohamerski@hotmail.com>
2021-12-21 18:55:01 -0300

    🎨 Add disabled option and tooltip for AppDropdownItem

--- src/components/AppDropdownItem.vue
+++ src/components/AppDropdownItem.vue
@@ -1,5 +1,10 @@
 <script>
+import { TippyComponent } from 'vue-tippy'
+
 export default {
+  components: {
+    Tippy: TippyComponent
+  },
   props: {
     text: {
       type: String,
@@ -20,40 +25,92 @@ export default {
     href: {
       type: String,
       default: '#'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    tooltip: {
+      type: String,
+      default: ''
+    },
+    disabledMessage: {
+      type: [String, Boolean],
+      default: false
     }
   },
   data () {
     return {
       active: false
     }
+  },
+  computed: {
+    label () {
+      return this.$slots.text || this.text
+    },
+    classes () {
+      return [
+        this.itemClass,
+        {
+          'disabled pointer-events-none': this.isDisabled
+        }
+      ]
+    },
+    isDisabled () {
+      return this.disabled || this.disabledMessage
+    },
+    tooltipText () {
+      return this.tooltip || this.disabledMessage
+    },
+    iconClass () {
+      if (this.isDisabled) {
+        return 'text-grey'
+      }
+
+      if (this.active) {
+        return 'text-white'
+      }
+
+      return `text-${this.iconColor}`
+    }
   }
 }
 </script>
 <template>
-  <li v-on="$listeners">
-    <a
-      class="dropdown-item"
-      :class="itemClass"
-      :href="href"
-      @mousedown="active = true"
-      @mouseup="active = false"
+  <li>
+    <Tippy
+      v-if="tooltipText"
+      :to="'dropdownTooltip__' + label"
     >
-      <FontAwesomeIcon
-        v-if="icon"
-        :class="{
-          [`text-${iconColor}`]: !active,
-          'text-white': active
-        }"
-        fixed-width
-        :icon="icon"
-      />
+      {{ tooltipText }}
+    </Tippy>
 
-      <template v-if="$slots.text">
-        <slot name="text" />
-      </template>
-      <template v-else>
-        {{ text }}
-      </template>
-    </a>
+    <span
+      class="d-block"
+      :name="'dropdownTooltip__' + label"
+    >
+      <a
+        :disabled="isDisabled"
+        class="dropdown-item"
+        :class="classes"
+        :href="href"
+        v-on="$listeners"
+        @mousedown="active = true"
+        @mouseup="active = false"
+      >
+        <FontAwesomeIcon
+          v-if="icon"
+          :class="iconClass"
+          fixed-width
+          :icon="icon"
+        />
+        <template v-if="$slots.text">
+          <slot name="text" />
+        </template>
+        <template v-else>
+          {{ text }}
+        </template>
+      </a>
+    </span>
   </li>
 </template>
