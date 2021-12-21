c0d937a0dd519b0fa086f4f88011e43471ad5cd2
João Antônio <joaohamerski@hotmail.com>
2021-12-21 19:11:23 -0300

    🎨 Add OrderHeader dropdown options and adjustments

--- src/views/orders/show/OrderHeader.vue
+++ src/views/orders/show/OrderHeader.vue
@@ -6,10 +6,30 @@ import {
   faEdit,
   faTrashAlt,
   faTimesCircle,
-  faExchangeAlt
+  faExchangeAlt,
+  faBoxOpen
 } from '@fortawesome/free-solid-svg-icons'
 
 export default {
+  chimera: {
+    _toggleOrder () {
+      return {
+        url: `/api/clients/${this.clientKey}/orders/${this.orderKey}/toggle-order`,
+        method: 'POST',
+        on: {
+          success () {
+            if (!this.order.closed_at) {
+              this.$toast.success('Pedido fechado!')
+            } else {
+              this.$toast.success('Pedido reaberto!')
+            }
+
+            this.$emit('refresh')
+          }
+        }
+      }
+    }
+  },
   props: {
     order: {
       type: Object,
@@ -25,8 +45,32 @@ export default {
         faEdit,
         faTrashAlt,
         faTimesCircle,
-        faExchangeAlt
+        faExchangeAlt,
+        faBoxOpen
       }
+    }
+  },
+  computed: {
+    clientKey () {
+      return this.$route.params.clientKey
+    },
+    orderKey () {
+      return this.$route.params.orderKey
+    },
+    paymentBtnDisabledMessage () {
+      if (!this.order) {
+        return true
+      }
+
+      if (this.order.states.includes('PAID')) {
+        return 'Pedido já pago'
+      }
+
+      if (this.order.states.includes('CLOSED')) {
+        return 'Não é possível registrar pagamento em pedidos fechados.'
+      }
+
+      return false
     }
   },
   methods: {
@@ -38,6 +82,12 @@ export default {
           order: this.$route.params.orderKey
         }
       })
+    },
+    toggleOrder () {
+      this.$chimera._toggleOrder.fetch()
+    },
+    onDeleteOrderClick () {
+      this.$emit('open-delete-order-modal')
     }
   }
 }
@@ -50,9 +100,8 @@ export default {
         color="success"
         outlined
         :icon="icons.faDollarSign"
-        :disabled="order.state === 'PAID'"
-        :tooltip="order.state === 'PAID' && 'Pedido já pago'"
-        @click="$emit('open-modal', {isEdit: false})"
+        :disabled-message="paymentBtnDisabledMessage"
+        @click="$emit('open-payment-modal', {isEdit: false})"
       >
         Adicionar pagamento
       </AppButton>
@@ -61,6 +110,8 @@ export default {
       <AppButton
         class="me-1"
         :icon="icons.faFileInvoice"
+        :disabled="!order"
+        @click="$emit('open-report-modal')"
       >
         Gerar relatório
       </AppButton>
@@ -69,37 +120,45 @@ export default {
         id="dropdownOptions"
         data-bs-toggle="dropdown"
         :icon="icons.faCog"
+        :disabled="!order"
       />
 
       <ul
+        v-if="order"
         class="dropdown-menu"
         aria-labelledby="dropdownOptions"
       >
         <AppDropdownItem
+          :disabled-message="order.states.includes('CLOSED') && 'Não é possível editar pedidos fechados.'"
           :icon="icons.faEdit"
           text="Editar"
           icon-color="primary"
-          @click="redirectToOrderEdit"
+          @click.prevent="redirectToOrderEdit"
         />
 
         <AppDropdownItem
           :icon="icons.faTrashAlt"
           text="Excluir"
           icon-color="danger"
+          @click="onDeleteOrderClick"
         />
 
         <div class="dropdown-divider" />
 
         <AppDropdownItem
+          :disabled-message="order.states.includes('CLOSED') && 'Não é possível alterar status de pedidos fechados.'"
           :icon="icons.faExchangeAlt"
           text="Alterar status"
           icon-color="primary"
+          @click="$emit('open-status-modal')"
         />
 
         <AppDropdownItem
-          :icon="icons.faTimesCircle"
-          text="Fechar pedido"
+          :disabled-message="!order.states.includes('PAID') && 'Não é possível fechar pedidos com pendência financeira.'"
+          :icon="!order.states.includes('CLOSED') ? icons.faTimesCircle : icons.faBoxOpen"
+          :text="!order.states.includes('CLOSED') ? 'Fechar pedido' : 'Reabrir pedido'"
           icon-color="primary"
+          @click.prevent="toggleOrder"
         />
       </ul>
     </div>
