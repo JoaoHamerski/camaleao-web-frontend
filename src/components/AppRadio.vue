c739ecc81026ce5e1ce36d3851c4f4c23b7ad78b
João Antônio <joaohamerski@hotmail.com>
2021-12-25 13:31:12 -0300

    ✨ Add new AppRadio component

---
+++ src/components/AppRadio.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  props: {
+    value: {
+      type: [String, Number],
+      default: ''
+    },
+    options: {
+      type: Array,
+      default: () => ([])
+    },
+    align: {
+      type: String,
+      default: 'horizontal'
+    }
+  },
+  computed: {
+    alignmentClass () {
+      if (this.align === 'horizontal') {
+        return 'form-check-inline'
+      }
+
+      if (this.align === 'vertical') {
+        return ''
+      }
+
+      return ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div
+      v-for="option in options"
+      :key="option.id"
+      class="form-check"
+      :class="alignmentClass"
+    >
+      <input
+        :id="`radio_${option.id}`"
+        class="form-check-input"
+        type="radio"
+        :name="`radio_${option.id}`"
+        :value="option.value"
+        :checked="option.value === value"
+        :disabled="option.disabled"
+        @change="$emit('input', $event.target.value)"
+      >
+      <label
+        class="form-check-label"
+        :for="`radio_${option.id}`"
+      >
+        {{ option.label }}
+      </label>
+    </div>
+  </div>
+</template>
