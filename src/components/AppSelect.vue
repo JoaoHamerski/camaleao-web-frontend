29e0ec0eea3329661655d79ae6d9aaa12e8d4449
João Antônio <joaohamerski@hotmail.com>
2022-01-01 16:26:29 -0300

    🎨 Improve AppSelect component

--- src/components/AppSelect.vue
+++ src/components/AppSelect.vue
@@ -2,13 +2,19 @@
 import { isBoolean } from 'lodash-es'
 
 import Multiselect from 'vue-multiselect'
+import { TippyComponent } from 'vue-tippy'
 
 export default {
   components: {
-    Multiselect
+    Multiselect,
+    Tippy: TippyComponent
   },
   props: {
     value: undefined,
+    defaultMargin: {
+      type: Boolean,
+      default: true
+    },
     name: {
       type: String,
       default: null
@@ -25,13 +31,13 @@ export default {
       type: String,
       default: 'Selecione uma opção'
     },
+    tooltip: {
+      type: [Boolean, String],
+      default: ''
+    },
     optional: {
       type: Boolean,
       default: false
-    },
-    options: {
-      type: Array,
-      default: () => []
     },
     hint: {
       type: String,
@@ -44,6 +50,12 @@ export default {
     },
     hasHint () {
       return !!(this.hint || this.$slots.hint)
+    },
+    hasNoOptionsSlot () {
+      return !!this.$slots.noOptions
+    },
+    hasNoResultSlot () {
+      return !!this.$slots.noResult
     }
   },
   methods: {
@@ -53,8 +65,9 @@ export default {
 </script>
 
 <template>
-  <div class="mb-3">
+  <div :class="{'mb-3': defaultMargin}">
     <label
+      v-if="$slots.default"
       class="form-label"
       :for="name"
     >
@@ -65,26 +78,48 @@ export default {
       >(opcional)</span>
     </label>
 
-    <Multiselect
-      :id="name"
-      :value="value"
-      :custom-label="customLabel"
-      :placeholder="placeholder"
-      :options="options"
-      :class="hasError && 'is-invalid'"
-      select-label="Selecionar"
-      selected-label="Selecionado"
-      deselect-label="Remover"
-      v-on="$listeners"
+    <Tippy
+      v-if="tooltip"
+      :to="`tippy__${name}`"
     >
-      <template #noOptions>
-        Lista está vazia.
-      </template>
-
-      <template #noResult>
-        Nenhum resultado encontrado.
-      </template>
-    </Multiselect>
+      {{ tooltip }}
+    </Tippy>
+    <span
+      class="d-block"
+      :name="`tippy__${name}`"
+    >
+      <Multiselect
+        :id="name"
+        :name="name"
+        :value="value"
+        :custom-label="customLabel"
+        :placeholder="placeholder"
+        :class="hasError && 'is-invalid'"
+        select-label="Selecionar"
+        selected-label="Selecionado"
+        deselect-label="Remover"
+        autocomplete="off"
+        v-bind="$attrs"
+        v-on="$listeners"
+      >
+        <template #noOptions>
+          <template v-if="!hasNoOptionsSlot">
+            A lista está vazia
+          </template>
+          <template v-else>
+            <slot name="noOptions" />
+          </template>
+        </template>
+        <template #noResult>
+          <template v-if="!hasNoResultSlot">
+            Nenhum resultado encontrado.
+          </template>
+          <template>
+            <slot name="noResult" />
+          </template>
+        </template>
+      </Multiselect>
+    </span>
     <div
       v-if="hasError && !isBoolean(error)"
       class="small text-danger mb-n1"
