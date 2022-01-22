5e3bd09fbf7dee13542a5f207f44b3d9a6a26eac
João Antônio <joaohamerski@hotmail.com>
2022-01-22 07:45:00 -0300

    🎨 Export v-viewer config

--- src/components/AppViewer/AppViewer.vue
+++ src/components/AppViewer/AppViewer.vue
@@ -1,6 +1,21 @@
 <script>
 import 'viewerjs/dist/viewer.css'
 import { directive as viewer } from 'v-viewer'
+export const config = {
+  inline: false,
+  button: true,
+  navbar: false,
+  title: true,
+  toolbar: false,
+  tooltip: true,
+  movable: true,
+  zoomable: true,
+  rotatable: false,
+  scalable: true,
+  transition: true,
+  fullscreen: true,
+  keyboard: true
+}
 
 export default {
   directives: {
@@ -8,21 +23,7 @@ export default {
   },
   data () {
     return {
-      config: {
-        inline: false,
-        button: true,
-        navbar: false,
-        title: true,
-        toolbar: false,
-        tooltip: true,
-        movable: true,
-        zoomable: true,
-        rotatable: false,
-        scalable: true,
-        transition: true,
-        fullscreen: true,
-        keyboard: true
-      }
+      config
     }
   }
 }
