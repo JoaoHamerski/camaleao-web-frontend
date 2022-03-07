26a25964331b950b2ea83617c93bcd44529f6377
João Hamerski <joaohamerski@hotmail.com>
2022-03-07 11:12:40 -0300

    ♻️ Refact and improve structure of ClientCard component

--- src/views/clients/partials/ClientCard.vue
+++ src/views/clients/partials/ClientCard.vue
@@ -1,15 +1,18 @@
 <script>
-import { faUser, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { isEmpty } from 'lodash-es'
-import { formatPhone } from '@/utils/formatters'
 
-import ClientCardItem from './ClientCardItem'
 import ClientModalEdit from './ClientModalEdit'
+import ClientCardItems from './ClientCardItems'
+import ClientCardFooter from './ClientCardFooter'
+import ClientCardNotExist from './ClientCardNotExist'
 
 export default {
   components: {
-    ClientCardItem,
-    ClientModalEdit
+    ClientModalEdit,
+    ClientCardItems,
+    ClientCardFooter,
+    ClientCardNotExist
   },
   props: {
     client: {
@@ -23,6 +26,10 @@ export default {
     clientKey: {
       type: [String, Number],
       default: null
+    },
+    isLoading: {
+      type: Boolean,
+      required: true
     }
   },
   data () {
@@ -32,41 +39,18 @@ export default {
         client: null
       },
       icons: {
-        faUser,
-        faEdit,
-        faTrashAlt
+        faUser
       }
     }
   },
   computed: {
     key () {
       return this.clientKey || this.$route.params.clientKey
-    },
-    clientCityInfo () {
-      const city = this.client.city?.name
-      const state = this.client.city?.state?.abbreviation
-
-      if (!city) {
-        return 'N/A'
-      }
-
-      return `${city} - ${state || 'N/A'}`
-    },
-    clientBranchInfo () {
-      const city = this.client.branch?.city?.name
-      const state = this.client.branch?.city?.state?.name
-
-      if (!city) {
-        return 'N/A'
-      }
-
-      return `${city} - ${state || 'N/A'}`
     }
   },
   methods: {
-    formatPhone,
     isEmpty,
-    onEditClientClick () {
+    onEditClient () {
       this.clientModalEdit.client = this.client
       this.clientModalEdit.value = true
     },
@@ -79,97 +63,48 @@ export default {
 </script>
 
 <template>
-  <keep-alive>
-    <AppCard
-      color="success"
+  <AppCard
+    color="success"
+  >
+    <template #header>
+      <h6 class="mb-0 fw-bold">
+        <FontAwesomeIcon
+          class="fa-fw"
+          :icon="icons.faUser"
+        />
+        Cliente
+      </h6>
+    </template>
+
+    <template
+      v-if="!isEmpty(client)"
+      #body
     >
-      <template #header>
-        <h6 class="mb-0 fw-bold">
-          <FontAwesomeIcon
-            class="fa-fw"
-            :icon="icons.faUser"
-          /> Cliente
-        </h6>
-      </template>
-      <template
-        v-if="!isEmpty(client)"
-        #body
-      >
-        <ClientModalEdit
-          v-model="clientModalEdit.value"
-          :client="clientModalEdit.client"
-          @success="onEditSuccess"
-        />
+      <ClientModalEdit
+        v-model="clientModalEdit.value"
+        :client="clientModalEdit.client"
+        @success="onEditSuccess"
+      />
 
-        <ClientCardItem
-          label="Nome"
-          :text="client.name"
-        />
-        <hr>
-        <ClientCardItem
-          label="Telefone/Celular"
-          :text="formatPhone(client.phone)"
-        />
-        <hr>
-        <ClientCardItem
-          label="Cidade"
-          :text="clientCityInfo"
-        />
-        <hr>
-        <ClientCardItem
-          label="Filial"
-          :text="clientBranchInfo"
-        />
-        <hr>
-        <ClientCardItem
-          label="Transportadora"
-          :text="$helpers.fallback(client.shipping_company, 'name')"
-        />
-        <hr>
-        <ClientCardItem
-          :color="client.total_owing > 0 ? 'danger' : 'success'"
-          label="Total devendo"
-        >
-          <template #text>
-            <!-- eslint-disable-next-line vue/no-v-html -->
-            <span v-html="$helpers.toBRL(client.total_owing, true)" />
-          </template>
-        </ClientCardItem>
+      <ClientCardItems :client="client" />
 
-        <hr>
+      <hr>
 
-        <div class="text-subtitle">
-          <div class="mb-1">
-            <span
-              class="link-primary clickable text-decoration-none"
-              @click.prevent="onEditClientClick"
-            >
-              <FontAwesomeIcon
-                :icon="icons.faEdit"
-                fixed-width
-              />
-              Editar dados
-            </span>
-          </div>
-          <div>
-            <span class="link-danger clickable">
-              <FontAwesomeIcon
-                :icon="icons.faTrashAlt"
-                fixed-width
-              />
-              Excluir cliente
-            </span>
-          </div>
-        </div>
-      </template>
-      <template
-        v-else
-        #body
-      >
-        <div class="py-5">
-          <AppLoading />
-        </div>
-      </template>
-    </AppCard>
-  </keep-alive>
+      <ClientCardFooter @edit-client="onEditClient" />
+    </template>
+    <template
+      v-else-if="isLoading"
+      #body
+    >
+      <div class="py-5">
+        <AppLoading />
+      </div>
+    </template>
+    <template
+      v-else-if="isEmpty(client)"
+      #body
+    >
+      <ClientCardNotExist />
+    </template>
+  </AppCard>
 </template>
---
+++ src/views/clients/partials/ClientCardItems.vue
@@ -0,0 +1,79 @@
+<script>
+import ClientCardItem from './ClientCardItem'
+import { formatPhone } from '@/utils/formatters'
+
+export default {
+  components: {
+    ClientCardItem
+  },
+  props: {
+    client: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  computed: {
+    clientCityInfo () {
+      const city = this.client.city?.name
+      const state = this.client.city?.state?.abbreviation
+
+      if (!city) {
+        return 'N/A'
+      }
+
+      return `${city} - ${state || 'N/A'}`
+    },
+    clientBranchInfo () {
+      const city = this.client.branch?.city?.name
+      const state = this.client.branch?.city?.state?.name
+
+      if (!city) {
+        return 'N/A'
+      }
+
+      return `${city} - ${state || 'N/A'}`
+    }
+  },
+  methods: {
+    formatPhone
+  }
+}
+</script>
+<template>
+  <div>
+    <ClientCardItem
+      label="Nome"
+      :text="client.name"
+    />
+    <hr>
+    <ClientCardItem
+      label="Telefone/Celular"
+      :text="formatPhone(client.phone)"
+    />
+    <hr>
+    <ClientCardItem
+      label="Cidade"
+      :text="clientCityInfo"
+    />
+    <hr>
+    <ClientCardItem
+      label="Filial"
+      :text="clientBranchInfo"
+    />
+    <hr>
+    <ClientCardItem
+      label="Transportadora"
+      :text="$helpers.fallback(client.shipping_company, 'name')"
+    />
+    <hr>
+    <ClientCardItem
+      :color="client.total_owing > 0 ? 'danger' : 'success'"
+      label="Total devendo"
+    >
+      <template #text>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <span v-html="$helpers.toBRL(client.total_owing, true)" />
+      </template>
+    </ClientCardItem>
+  </div>
+</template>
