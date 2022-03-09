7935374b0d39472444d60a5e669e5f3b3b23ed40
João Hamerski <joaohamerski@hotmail.com>
2022-03-09 18:30:45 -0300

    🎨 Updates AppFileModal component

---
+++ src/components/AppFileModal.vue
@@ -0,0 +1,46 @@
+<script>
+import ViewerFileModal from '@/components/AppViewer/ViewerFileModal'
+
+export default {
+  components: {
+    ViewerFileModal
+  },
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: Boolean,
+      default: false
+    },
+    src: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<template>
+  <ViewerFileModal
+    :id="id"
+    :value="value"
+    :src="src"
+    modal-dialog-class="modal-fullscreen"
+    v-on="$listeners"
+  >
+    <template #title>
+      <template v-if="title !== ''">
+        {{ title }}
+      </template>
+      <template v-else>
+        <slot name="title" />
+      </template>
+    </template>
+  </ViewerFileModal>
+</template>
