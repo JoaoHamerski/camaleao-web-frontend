36b29f7d52b52945866e06dc79923ec4ec05caf2
João Hamerski <joaohamerski@hotmail.com>
2022-03-09 18:36:12 -0300

    ✨ Implement report generation logic on TheExpensesReportCard

--- src/views/expenses/index/TheExpensesReportCard.vue
+++ src/views/expenses/index/TheExpensesReportCard.vue
@@ -1,31 +1,14 @@
 <script>
-import Form from '@/utils/Form'
-import { handleError } from '@/utils/forms'
 import { faClipboardList } from '@fortawesome/free-solid-svg-icons'
 import { maskDate } from '@/utils/masks'
-import ViewerFileModal from '@/components/AppViewer/ViewerFileModal'
-
-const EXPENSES_REPORT_PATH_URL = '/api/expenses/report'
+import { handleError } from '@/utils/forms'
+import Form from '@/utils/Form'
+import { expensesReport } from '@/graphql/Expense.gql'
 
 export default {
-  chimera: {
-    _validateReport () {
-      return {
-        method: 'POST',
-        url: 'api/expenses/report-validate',
-        on: {
-          error ({ error }) {
-            handleError(this, error)
-          }
-        }
-      }
-    }
-  },
-  components: {
-    ViewerFileModal
-  },
   data () {
     return {
+      isLoading: false,
       icons: {
         faClipboardList
       },
@@ -35,44 +18,34 @@ export default {
       },
       form: new Form({
         start_date: '',
-        end_date: ''
+        final_date: ''
       }),
       hasDateInterval: false,
       maskDate
     }
   },
-  computed: {
-    apiUrl () {
-      return this.$store.getters.apiURL
-    }
-  },
   methods: {
-    generateUrl (data) {
-      const url = new URL(this.apiUrl + EXPENSES_REPORT_PATH_URL)
-      url.search = new URLSearchParams(data)
+    async generateReport () {
+      const input = this.form.data()
 
-      return url.toString()
-    },
-    async generateReportModal () {
-      const data = this.form.data()
-
-      if (!this.hasDateInterval) {
-        data.end_date = ''
-      }
+      this.isLoading = true
 
       try {
-        this.form.errors.clear('*')
-
-        await this.$chimera._validateReport.fetch(true, {
-          params: data
+        const { data } = await this.$apollo.query({
+          query: expensesReport,
+          variables: {
+            start_date: input.start_date,
+            final_date: input.final_date
+          },
+          fetchPolicy: 'network-only'
         })
 
-        this.modal.src = this.generateUrl(data)
-        this.modal.value = true
-      } catch (error) {}
-    },
-    onModalHidden () {
-      this.modal.src = ''
+        this.$emit('report-generated', data.expensesReport)
+      } catch (error) {
+        handleError(this, error)
+      }
+
+      this.isLoading = false
     }
   }
 }
@@ -80,12 +53,6 @@ export default {
 
 <template>
   <div>
-    <ViewerFileModal
-      v-model="modal.value"
-      :src="modal.src"
-      @hidden="onModalHidden"
-    />
-
     <AppCard
       collapsible
       color="success"
@@ -102,7 +69,7 @@ export default {
       <template #body>
         <AppForm
           :form="form"
-          :on-submit="generateReportModal"
+          :on-submit="generateReport"
         >
           <div class="row">
             <div class="form-label fw-bold">
@@ -123,28 +90,31 @@ export default {
               class="col"
             >
               <AppInput
-                v-model="form.end_date"
-                name="end_date"
+                v-model="form.final_date"
+                name="final_date"
                 placeholder="dd/mm/aaaa"
                 type="date"
                 :mask="maskDate"
-                :error="form.errors.get('end_date')"
+                :error="form.errors.get('final_date')"
               />
             </div>
           </div>
+
+          <AppCheckbox
+            id="betweenDates"
+            v-model="hasDateInterval"
+          >
+            Por intervalo de datas
+          </AppCheckbox>
+
+          <AppButton
+            outlined
+            :loading="isLoading"
+            @click.prevent="generateReport"
+          >
+            Gerar relatório
+          </AppButton>
         </AppForm>
-        <AppCheckbox
-          id="betweenDates"
-          v-model="hasDateInterval"
-        >
-          Por intervalo de datas
-        </AppCheckbox>
-        <AppButton
-          outlined
-          @click="generateReportModal"
-        >
-          Gerar relatório
-        </AppButton>
       </template>
     </AppCard>
   </div>
