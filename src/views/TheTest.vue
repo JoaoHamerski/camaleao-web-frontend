7e195920fb6149d8a10d417454088701332d52bd
João Hamerski <joaohamerski@hotmail.com>
2022-03-14 18:41:41 -0300

    🧑‍💻 Add test view for component testing

---
+++ src/views/TheTest.vue
@@ -0,0 +1,17 @@
+<script>
+/**
+ * @file
+ *
+ * Arquivo para teste de componentes.
+ * Acessado em /testes
+ */
+export default {
+
+}
+</script>
+
+<template>
+  <div>
+    <!--  -->
+  </div>
+</template>
