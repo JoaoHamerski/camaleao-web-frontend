35c3f74cc281c7fe437ee425acee8c79cb150c32
João Hamerski <joaohamerski@hotmail.com>
2022-04-01 19:31:12 -0300

    💄 Wrap client form of order pre-registered on AppContainer

--- src/views/orders/form/OrderFormClient.vue
+++ src/views/orders/form/OrderFormClient.vue
@@ -64,29 +64,30 @@ export default {
 </script>
 
 <template>
-  <div class="mb-4">
-    <h5 class="text-secondary fw-bold">
+  <AppContainer class="mb-4">
+    <template #title>
       Cliente do pedido
-    </h5>
+    </template>
+    <template #body>
+      <AppSelect
+        id="client_id"
+        :value="form.client_id"
+        name="client_id"
+        track-by="id"
+        :options="clients"
+        :custom-label="clientCustomLabel"
+        :loading="isLoadingClients"
+        placeholder="Busque um cliente..."
+        :error="form.errors.get('client_id')"
+        @input="form.set({client_id: $event})"
+        @search-change="onClientSearch"
+      >
+        <template #noOptions>
+          Faça uma busca para exibir resultados
+        </template>
 
-    <AppSelect
-      id="client_id"
-      :value="form.client_id"
-      name="client_id"
-      track-by="id"
-      :options="clients"
-      :custom-label="clientCustomLabel"
-      :loading="isLoadingClients"
-      placeholder="Busque um cliente..."
-      :error="form.errors.get('client_id')"
-      @input="form.set({client_id: $event})"
-      @search-change="onClientSearch"
-    >
-      <template #noOptions>
-        Faça uma busca para exibir resultados
-      </template>
-
-      Nome ou telefone do cliente:
-    </AppSelect>
-  </div>
+        Nome ou telefone do cliente:
+      </AppSelect>
+    </template>
+  </AppContainer>
 </template>
