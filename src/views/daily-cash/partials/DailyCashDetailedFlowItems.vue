77e43181fb32f5c01be4ada492d96eb904eae7b6
João Hamerski <joaohamerski@hotmail.com>
2022-06-20 17:54:57 -0300

    Update

--- src/views/daily-cash/partials/DailyCashDetailedFlowItems.vue
+++ src/views/daily-cash/partials/DailyCashDetailedFlowItems.vue
@@ -79,47 +79,66 @@ export default {
             </div>
             <div>
               <div>
-                <FontAwesomeIcon
-                  :icon="icons.faTshirt"
-                  fixed-width
-                />
-                Pedidos com peças:
-              </div>
-              <div class="ms-4 fw-bold">
-                <div class="row gx-0">
-                  <div class="col-2">
-                    &lt; 5
-                  </div>
-                  <div class="col-3 text-success">
-                    ({{ item.shirts_details.less_than_five.quantity }}):
-                  </div>
-                  <div class="col text-info">
-                    {{ $helpers.toBRL(item.shirts_details.less_than_five.value) }}
-                  </div>
-                </div>
-                <div class="row gx-0">
-                  <div class="col-2">
-                    5~10
-                  </div>
-                  <div class="col-3 text-success">
-                    ({{ item.shirts_details.between_five_and_ten.quantity }}):
-                  </div>
-                  <div class="col text-info">
-                    {{ $helpers.toBRL(item.shirts_details.between_five_and_ten.value) }}
-                  </div>
-                </div>
-
-                <div class="row gx-0">
-                  <div class="col-2">
-                    &gt; 10
-                  </div>
-                  <div class="col-3 text-success">
-                    ({{ item.shirts_details.more_than_ten.quantity }}):
-                  </div>
-                  <div class="col text-info">
-                    {{ $helpers.toBRL(item.shirts_details.more_than_ten.value) }}
-                  </div>
-                </div>
+                <table class="table table-sm">
+                  <thead>
+                    <tr
+                      class="small"
+                    >
+                      <th colspan="4">
+                        <FontAwesomeIcon
+                          :icon="icons.faTshirt"
+                          fixed-width
+                        />
+                        Pedidos com peças:
+                      </th>
+                    </tr>
+                    <tr class="small">
+                      <th class="table-primary">
+                        Intervalo
+                      </th>
+                      <th>
+                        <span
+                          v-tippy
+                          content="Quantidade de PEDIDOS que estão no INTERVALO de peças"
+                        >Qtd.</span>
+                      </th>
+                      <th>Total</th>
+                      <th>Peças</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr class="small">
+                      <td class="fw-bold table-primary">
+                        &lt; 5
+                      </td>
+                      <td>{{ item.shirts_details.less_than_five.count }}</td>
+                      <td nowrap>
+                        {{ $helpers.toBRL(item.shirts_details.less_than_five.value) }}
+                      </td>
+                      <td>{{ item.shirts_details.less_than_five.quantity }}</td>
+                    </tr>
+                    <tr class="small">
+                      <td class="fw-bold table-primary">
+                        5 ~ 10
+                      </td>
+                      <td>{{ item.shirts_details.between_five_and_ten.count }}</td>
+                      <td nowrap>
+                        {{ $helpers.toBRL(item.shirts_details.between_five_and_ten.value) }}
+                      </td>
+                      <td>{{ item.shirts_details.between_five_and_ten.quantity }}</td>
+                    </tr>
+                    <tr class="small">
+                      <td class="fw-bold table-primary">
+                        &gt; 10
+                      </td>
+                      <td>{{ item.shirts_details.more_than_ten.count }}</td>
+                      <td nowrap>
+                        {{ $helpers.toBRL(item.shirts_details.more_than_ten.value) }}
+                      </td>
+                      <td>{{ item.shirts_details.more_than_ten.quantity }}</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
