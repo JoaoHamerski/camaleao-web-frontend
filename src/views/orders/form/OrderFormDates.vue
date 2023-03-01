a0db61e09588e6ee23eda4347936e3ca1522a3a6
João Hamerski <joaohamerski@hotmail.com>
2023-03-01 09:42:25 -0300

    Update

--- src/views/orders/form/OrderFormDates.vue
+++ src/views/orders/form/OrderFormDates.vue
@@ -26,36 +26,6 @@ export default {
       <div class="row">
         <div class="col-12 col-sm">
           <AppInput
-            id="print_date"
-            :value="form.print_date"
-            type="date"
-            name="print_date"
-            placeholder="dd/mm/aaaa"
-            numeric
-            :mask="maskDate"
-            :error="form.errors.get('print_date')"
-            @input="form.set({print_date: $event})"
-          >
-            Data de estampa
-          </AppInput>
-        </div>
-        <div class="col-12 col-sm">
-          <AppInput
-            id="seam_date"
-            :value="form.seam_date"
-            type="date"
-            name="seam_date"
-            placeholder="dd/mm/aaaa"
-            :mask="maskDate"
-            numeric
-            :error="form.errors.get('seam_date')"
-            @input="form.set({seam_date: $event})"
-          >
-            Data de costura
-          </AppInput>
-        </div>
-        <div class="col-12 col-sm">
-          <AppInput
             id="delivery_date"
             :value="form.delivery_date"
             name="delivery_date"
