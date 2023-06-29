4f13a2f221e7d43a44e18a53cb1d656031765d7f
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2023-06-29 17:40:27 -0300

    Update

--- src/views/garments/index/garment-items/GarmentItemModalBody.vue
+++ src/views/garments/index/garment-items/GarmentItemModalBody.vue
@@ -1,7 +1,9 @@
 <script>
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { ReorderGarmentOption } from '@/graphql/Garment.gql'
 
 import GarmentItemModalItems from './GarmentItemModalItems.vue'
+import { snakeCase } from 'lodash-es'
 
 export default {
   components: {
@@ -18,6 +20,8 @@ export default {
       faPlus,
     },
     newItem: false,
+    reorder: false,
+    isLoading: false,
   }),
   methods: {
     onNewItemClick () {
@@ -25,6 +29,35 @@ export default {
     },
     onCancelNewItem () {
       this.newItem = false
+    },
+    async onReorderConcluded () {
+      this.isLoading = true
+
+      const items = this.$refs.garmentItemModalItems.items.map((item, index) => ({
+        id: item.id,
+        order: index
+      }))
+
+      const option = snakeCase(this.item.queryName.substring(0, this.item.queryName.length - 1))
+
+      const input = {
+        option: option.toUpperCase(),
+        items
+      }
+
+      try {
+        await this.$apollo.mutate({
+          mutation: ReorderGarmentOption,
+          variables: { input }
+        })
+
+        this.$toast.success('Itens reordenados!')
+        this.reorder = false
+      } catch (error) {
+        this.$toast.error('Ops! Algo deu errado.')
+      }
+
+      this.isLoading = false
     }
   }
 }
@@ -32,7 +65,7 @@ export default {
 
 <template>
   <div>
-    <div class="mb-3">
+    <div class="d-flex justify-content-between mb-3">
       <AppButton
         outlined
         color="success"
@@ -42,22 +75,44 @@ export default {
       >
         Novo item
       </AppButton>
+      <AppCheckboxSwitch
+        id="reordeing"
+        v-model="reorder"
+      >
+        Reordenar
+      </AppCheckboxSwitch>
     </div>
 
     <GarmentItemModalItems
+      ref="garmentItemModalItems"
       class="my-3"
       :item="item"
       :new-item="newItem"
+      :reorder="reorder"
       @cancel-new-item="onCancelNewItem"
     />
 
-    <div class="text-end">
-      <AppButton
-        color="light"
-        data-bs-dismiss="modal"
-      >
-        Fechar
-      </AppButton>
+    <div class="d-flex justify-content-between text-end">
+      <div>
+        <AppButton
+          v-if="reorder"
+          color="success"
+          btn-class="fw-bold"
+          :loading="isLoading"
+          @click.prevent="onReorderConcluded"
+        >
+          Concluir reordenação
+        </AppButton>
+      </div>
+      <div>
+        <AppButton
+          color="light"
+          data-bs-dismiss="modal"
+          :disabled="isLoading"
+        >
+          Fechar
+        </AppButton>
+      </div>
     </div>
   </div>
 </template>
--- src/views/garments/index/garment-items/GarmentItemModalItems.vue
+++ src/views/garments/index/garment-items/GarmentItemModalItems.vue
@@ -1,12 +1,14 @@
 <script>
-import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faBars } from '@fortawesome/free-solid-svg-icons'
 import { getOperationName } from '@apollo/client/utilities'
 
 import GarmentItemForm from './GarmentItemForm.vue'
+import Draggable from 'vuedraggable'
 
 export default {
   components: {
-    GarmentItemForm
+    GarmentItemForm,
+    Draggable
   },
   apollo: {
     items () {
@@ -26,6 +28,10 @@ export default {
     newItem: {
       type: Boolean,
       default: false
+    },
+    reorder: {
+      type: Boolean,
+      default: false
     }
   },
   data: () => ({
@@ -34,12 +40,21 @@ export default {
     },
     items: [],
     icons: {
-      faEdit
-    }
+      faEdit,
+      faBars
+    },
+    drag: false,
   }),
   computed: {
     isQueryLoading () {
       return this.$apollo.queries.items.loading
+    },
+    draggableAttrs () {
+      return {
+        animation: 200,
+        disabled: !this.reorder,
+        ghostClass: "ghost"
+      }
     }
   },
   methods: {
@@ -90,30 +105,50 @@ export default {
         Nenhum item para exibir
       </div>
 
-      <li
-        v-for="_item in items"
-        :key="_item.id"
-        class="list-group-item d-flex justify-content-between align-items-center"
+      <Draggable
+        v-model="items"
+        v-bind="draggableAttrs"
+        handle=".draggable-handler"
+        @start="drag = true"
+        @end="drag = false"
       >
-        <template v-if="isEdit.id === _item.id">
-          <GarmentItemForm
-            :item="_item"
-            :item-query-name="item.queryName"
-            :submit="submit"
-            @cancel="onCancelEdit"
-          />
-        </template>
-        <template v-else>
-          <span>{{ _item.name }}</span>
-
-          <AppButton
-            btn-class="btn-sm"
-            :icon="icons.faEdit"
-            outlined
-            @click.prevent="onEditClick(_item)"
-          />
-        </template>
-      </li>
+        <TransitionGroup
+          tag="div"
+          type="transition"
+          :name="drag ? 'flip-list' : null"
+        >
+          <li
+            v-for="_item in items"
+            :key="_item.id"
+            class="list-group-item d-flex justify-content-between align-items-center"
+          >
+            <template v-if="isEdit.id === _item.id">
+              <GarmentItemForm
+                :item="_item"
+                :item-query-name="item.queryName"
+                :submit="submit"
+                @cancel="onCancelEdit"
+              />
+            </template>
+            <template v-else>
+              <span>{{ _item.name }}</span>
+              <FontAwesomeIcon
+                v-if="reorder"
+                :key="`icon__${_item.id}`"
+                :icon="icons.faBars"
+                class="me-3 my-2 draggable-handler"
+              />
+              <AppButton
+                v-if="!reorder"
+                btn-class="btn-sm"
+                :icon="icons.faEdit"
+                outlined
+                @click.prevent="onEditClick(_item)"
+              />
+            </template>
+          </li>
+        </TransitionGroup>
+      </Draggable>
 
       <li
         v-if="newItem"
